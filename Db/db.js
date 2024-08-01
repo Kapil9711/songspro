3c9e76cd4853a1fea08a34e85a3a868aee087548
kapil <kapil.personal9711@gmail.com>
2024-08-01 10:48:46 +0530

    added connectDb and listion on port 8000

--- Db/db.js
+++ Db/db.js
@@ -1,1 +1,13 @@
-console.log(process.env.NODE_ENV);
+import { connect } from "mongoose";
+
+const connectDB = async (connectionStr) => {
+  try {
+    await connect(connectionStr);
+    console.log("connection to db successfull");
+  } catch (error) {
+    console.log({ message: "error in connecting to db", error: error.message });
+    throw new Error();
+  }
+};
+
+export default connectDB;
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 import "dotenv/config";
 import express from "express";
 const app = express();
+
+/*
+ ==============
+ 1. connecting to db
+ 2. starting server
+ ==============
+*/
+import connectDB from "./Db/db.js";
+
+const PORT = process.env.PORT || 8000;
+const MODE = process.env.NODE_ENV;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () =>
+      console.log("Listening on Port", PORT, "in", MODE, "mode")
+    );
+  } catch (error) {
+    console.log("Aborting server due to problem in connection to db");
+  }
+};
+
+start();
