35f4d228a812beeaea0acd40822bd03deb020108
kapil <kapil.personal9711@gmail.com>
2024-08-29 15:16:50 +0530

    new

--- public/assets/LandingPage-maH3CYr3.js
+++
@@ -1,8 +0,0 @@
-import{e as o,r as n,j as e,A as r}from"./index-1wBAx9-n.js";import{d as c,u as i,g,E as d}from"./index-BBT2yVxp.js";const x="/assets/landingPagebg-Bi3d_ctO.jpg",l="/assets/bgm-Dyhjn3Ps.mp3",u=c.div`
-  height: 100vh;
-  background-image: url(${x});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  /* object-fit: cover; */
-`,m=()=>{const t=o(),s=n.useRef(null);return i(()=>{g.from(".heading",{duration:2,opacity:0,y:-30,delay:1,ease:d.easeOut,stagger:.2})}),n.useEffect(()=>{const a=document.getElementById("audio-playback");a.src=l,s.current.addEventListener("mousemove",()=>a.play())},[]),e.jsxs(u,{ref:s,className:"flex justify-center items-center flex-col gap-2 sm:gap-6",children:[e.jsxs("h1",{className:"heading text-white text-3xl  sm:text-6xl font-bold tracking-wide text-center max-w-[700px]",children:["Explore the Depths of"," ",e.jsx("span",{className:"heading text-orange-600 text-2xl sm:text-[55px] block w-fit mx-auto py-5",children:"Mindful Musings"})]}),e.jsx("button",{onClick:()=>{t("/auth")},className:"heading text-white bg-orange-600 py-3 sm:py-4 rounded-sm px-5 sm:px-12 text-xl sm:text-3xl font-bold hover:bg-orange-500",children:"Get Started"})]})},f=()=>{const t=o();return n.useEffect(()=>{(async()=>{(await r.isUserLoggedin()).success?t("/dashboard/content"):t("/")})()},[]),e.jsx("section",{children:e.jsx(m,{})})};export{f as default};
--- public/assets/Notify-Cghgtl9l.js
+++
@@ -1,1 +0,0 @@
-import{F as t}from"./index-1wBAx9-n.js";const s=(o,i,r)=>{o==="promise"?t[o](i,r,{position:"top-right"}):t[o](i,{position:"top-right"})};export{s as N};
